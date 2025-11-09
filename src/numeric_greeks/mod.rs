6f22be563db21fdb9db789852e7641596cff0565
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-11-09 20:43:56 +0000

    wip

--- src/numeric_greeks/mod.rs
+++ src/numeric_greeks/mod.rs
@@ -1,5 +1,6 @@
 mod constants;
 pub mod with_carry;
+pub mod with_dividend_yield;
 pub mod without_carry;
 
 pub use constants::DifferenceMethod;
---
+++ src/numeric_greeks/with_dividend_yield.rs
@@ -0,0 +1,443 @@
+/// Class for calculating numeric greeks for options using finite difference
+/// methods for the dividend yield style.
+use crate::numeric_greeks::DifferenceMethod;
+
+pub struct NumericGreeks {
+    /// fn price(S: 64, K: f64, T, r: f64, q: f64, v: f64) -> f64
+    pub price: Box<dyn Fn(f64, f64, f64, f64, f64, f64) -> f64>,
+}
+
+impl NumericGreeks {
+    pub fn new(price: impl Fn(f64, f64, f64, f64, f64, f64) -> f64 + 'static) -> Self {
+        NumericGreeks {
+            price: Box::new(price),
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn delta(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>,                  // = 0.01,
+        method: Option<DifferenceMethod>, // = 'central'
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        let method = method.unwrap_or(DifferenceMethod::Central);
+        match method {
+            DifferenceMethod::Central => {
+                ((self.price)(S + dS, K, T, r, q, v) - (self.price)(S - dS, K, T, r, q, v))
+                    / (2.0 * dS)
+            }
+            DifferenceMethod::Forward => {
+                ((self.price)(S + dS, K, T, r, q, v) - (self.price)(S, K, T, r, q, v)) / dS
+            }
+            DifferenceMethod::Backward => {
+                ((self.price)(S, K, T, r, q, v) - (self.price)(S - dS, K, T, r, q, v)) / dS
+            }
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn gamma(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>,                  // = 0.01,
+        method: Option<DifferenceMethod>, // = 'central'
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        let method = method.unwrap_or(DifferenceMethod::Central);
+        match method {
+            DifferenceMethod::Central => {
+                ((self.price)(S + dS, K, T, r, q, v) - 2.0 * (self.price)(S, K, T, r, q, v)
+                    + (self.price)(S - dS, K, T, r, q, v))
+                    / (dS * dS)
+            }
+            DifferenceMethod::Forward => {
+                ((self.price)(S + 2.0 * dS, K, T, r, q, v)
+                    - 2.0 * (self.price)(S + dS, K, T, r, q, v)
+                    + (self.price)(S, K, T, r, q, v))
+                    / (dS * dS)
+            }
+            DifferenceMethod::Backward => {
+                ((self.price)(S, K, T, r, q, v) - 2.0 * (self.price)(S - dS, K, T, r, q, v)
+                    + (self.price)(S - 2.0 * dS, K, T, r, q, v))
+                    / (dS * dS)
+            }
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn theta(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dT: Option<f64>,                  // = 1 / 365,
+        method: Option<DifferenceMethod>, // = 'central'
+    ) -> f64 {
+        let dT = dT.unwrap_or(1.0 / 365.0);
+        let method = method.unwrap_or(DifferenceMethod::Central);
+        match method {
+            DifferenceMethod::Central => {
+                ((self.price)(S, K, T - dT, r, q, v) - (self.price)(S, K, T + dT, r, q, v))
+                    / (2.0 * dT)
+            }
+            DifferenceMethod::Forward => {
+                ((self.price)(S, K, T, r, q, v) - (self.price)(S, K, T + dT, r, q, v)) / dT
+            }
+            DifferenceMethod::Backward => {
+                ((self.price)(S, K, T - dT, r, q, v) - (self.price)(S, K, T, r, q, v)) / dT
+            }
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn vega(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dv: Option<f64>,                  // = 0.001,
+        method: Option<DifferenceMethod>, // = 'central'
+    ) -> f64 {
+        let dv = dv.unwrap_or(0.001);
+        let method = method.unwrap_or(DifferenceMethod::Central);
+        match method {
+            DifferenceMethod::Central => {
+                ((self.price)(S, K, T, r, q, v + dv) - (self.price)(S, K, T, r, q, v - dv))
+                    / (2.0 * dv)
+            }
+            DifferenceMethod::Forward => {
+                ((self.price)(S, K, T, r, q, v + dv) - (self.price)(S, K, T, r, q, v)) / dv
+            }
+            DifferenceMethod::Backward => {
+                ((self.price)(S, K, T, r, q, v) - (self.price)(S, K, T, r, q, v - dv)) / dv
+            }
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn rho(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dr: Option<f64>,                  // = 0.001,
+        method: Option<DifferenceMethod>, //  = 'central'
+    ) -> f64 {
+        let dr = dr.unwrap_or(0.001);
+        let method = method.unwrap_or(DifferenceMethod::Central);
+        match method {
+            DifferenceMethod::Central => {
+                ((self.price)(S, K, T, r + dr, q, v) - (self.price)(S, K, T, r - dr, q, v))
+                    / (2.0 * dr)
+            }
+            DifferenceMethod::Forward => {
+                ((self.price)(S, K, T, r + dr, q, v) - (self.price)(S, K, T, r, q, v)) / dr
+            }
+            DifferenceMethod::Backward => {
+                ((self.price)(S, K, T, r, q, v) - (self.price)(S, K, T, r - dr, q, v)) / dr
+            }
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn carry(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dq: Option<f64>,                  // = 0.001,
+        method: Option<DifferenceMethod>, // = 'central'
+    ) -> f64 {
+        let dq = dq.unwrap_or(0.001);
+        let method = method.unwrap_or(DifferenceMethod::Central);
+        match method {
+            DifferenceMethod::Central => {
+                ((self.price)(S, K, T, r, q + dq, v) - (self.price)(S, K, T, r, q - dq, v))
+                    / (2.0 * dq)
+            }
+            DifferenceMethod::Forward => {
+                ((self.price)(S, K, T, r, q + dq, v) - (self.price)(S, K, T, r, q, v)) / dq
+            }
+            DifferenceMethod::Backward => {
+                ((self.price)(S, K, T, r, q, v) - (self.price)(S, K, T, r, q - dq, v)) / dq
+            }
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn elasticity(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        return ((self.price)(S + dS, K, T, r, q, v) - (self.price)(S - dS, K, T, r, q, v))
+            / (2.0 * dS)
+            * S
+            / (self.price)(S, K, T, r, q, v);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn speed(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        return ((self.price)(S + 2.0 * dS, K, T, r, q, v)
+            - 3.0 * (self.price)(S + dS, K, T, r, q, v)
+            + 3.0 * (self.price)(S, K, T, r, q, v)
+            - (self.price)(S - dS, K, T, r, q, v))
+            / (dS * dS * dS);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn deltap(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        ((self.price)(S * (1.0 + dS), K, T, r, q, v) - (self.price)(S * (1.0 - dS), K, T, r, q, v))
+            * 2.0
+            / S
+    }
+
+    #[allow(non_snake_case)]
+    pub fn gammap(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        S / 100.0
+            * ((self.price)(S + dS, K, T, r, q, v) - 2.0 * (self.price)(S, K, T, r, q, v)
+                + (self.price)(S - dS, K, T, r, q, v))
+            / (dS * dS)
+    }
+
+    #[allow(non_snake_case)]
+    pub fn vegap(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dv: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dv = dv.unwrap_or(0.01);
+        ((self.price)(S, K, T, r, q, v + dv) - (self.price)(S, K, T, r, q, v - dv)) * v / 0.1 / 2.0
+    }
+
+    #[allow(non_snake_case)]
+    pub fn vanna(
+        self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01,
+        dv: Option<f64>, // = 0.001
+    ) -> f64 {
+        // Also known as DdeltaDvol
+        let dS = dS.unwrap_or(0.01);
+        let dv = dv.unwrap_or(0.001);
+
+        ((self.price)(S + dS, K, T, r, q, v + dv)
+            - (self.price)(S + dS, K, T, r, q, v - dv)
+            - (self.price)(S - dS, K, T, r, q, v + dv)
+            + (self.price)(S - dS, K, T, r, q, v - dv))
+            / (4.0 * dS)
+            / dv
+    }
+
+    #[allow(non_snake_case)]
+    pub fn charm(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01,
+        dT: Option<f64>, // = 1 / 365
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        let dT = dT.unwrap_or(1.0 / 365.0);
+        // Also known as DdeltaDtime
+        ((self.price)(S + dS, K, T + dT, r, q, v)
+            - (self.price)(S + dS, K, T - dT, r, q, v)
+            - (self.price)(S - dS, K, T + dT, r, q, v)
+            + (self.price)(S - dS, K, T - dT, r, q, v))
+            / (4.0 * dS)
+            / -dT
+    }
+
+    #[allow(non_snake_case)]
+    pub fn dgamma_dvol(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dS: Option<f64>, // = 0.01,
+        dv: Option<f64>, // = 0.001
+    ) -> f64 {
+        let dS = dS.unwrap_or(0.01);
+        let dv = dv.unwrap_or(0.001);
+        ((self.price)(S + dS, K, T, r, q, v + dv) - 2.0 * (self.price)(S, K, T, r, q, v + dv)
+            + (self.price)(S - dS, K, T, r, q, v + dv)
+            - (self.price)(S + dS, K, T, r, q, v - dv)
+            + 2.0 * (self.price)(S, K, T, r, q, v - dv)
+            - (self.price)(S - dS, K, T, r, q, v - dv))
+            / (2.0 * dv * (dS * dS))
+    }
+
+    #[allow(non_snake_case)]
+    pub fn vomma(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dv: Option<f64>, // = 0.001,
+    ) -> f64 {
+        // DvegaDvol
+        let dv = dv.unwrap_or(0.001);
+        ((self.price)(S, K, T, r, q, v + dv) - 2.0 * (self.price)(S, K, T, r, q, v)
+            + (self.price)(S, K, T, r, q, v - dv))
+            / (dv * dv)
+    }
+
+    #[allow(non_snake_case)]
+    pub fn time_gamma(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dT: Option<f64>, // = 1 / 365,
+    ) -> f64 {
+        let dT = dT.unwrap_or(1.0 / 360.0);
+        return ((self.price)(S, K, T + dT, r, q, v) - 2.0 * (self.price)(S, K, T, r, q, v)
+            + (self.price)(S, K, T - dT, r, q, v))
+            / (dT * dT);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn futures_rho(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dr: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dr = dr.unwrap_or(0.01);
+        ((self.price)(S, K, T, r + dr, q, v) - (self.price)(S, K, T, r - dr, q, v)) / 2.0
+    }
+
+    #[allow(non_snake_case)]
+    pub fn rho2(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dq: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dq = dq.unwrap_or(0.01);
+        ((self.price)(S, K, T, r, q - dq, v) - (self.price)(S, K, T, r, q + dq, v)) / 2.0
+    }
+
+    #[allow(non_snake_case)]
+    pub fn strike_delta(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dK: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dK = dK.unwrap_or(0.01);
+        ((self.price)(S, K + dK, T, r, q, v) - (self.price)(S, K - dK, T, r, q, v)) / (2.0 * dK)
+    }
+
+    #[allow(non_snake_case)]
+    pub fn strike_gamma(
+        &self,
+        S: f64,
+        K: f64,
+        T: f64,
+        r: f64,
+        q: f64,
+        v: f64,
+        dK: Option<f64>, // = 0.01,
+    ) -> f64 {
+        let dK = dK.unwrap_or(0.01);
+        ((self.price)(S, K + dK, T, r, q, v) - 2.0 * (self.price)(S, K, T, r, q, v)
+            + (self.price)(S, K - dK, T, r, q, v))
+            / (dK * dK)
+    }
+}
