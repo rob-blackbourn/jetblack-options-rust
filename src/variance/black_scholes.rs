c6232b38ee178197110dc82fb04e94f865f9c9c3
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-11-30 14:31:46 +0000

    wip

--- src/variance/black_scholes.rs
+++ src/variance/black_scholes.rs
@@ -1,7 +1,5 @@
 //! Black Scholes variance analytic solutions
 
-use libm::{exp, log, sqrt};
-
 fn cdf(x: f64) -> f64 {
     crate::distributions::cdf(x, 0.0, 1.0)
 }
@@ -12,13 +10,13 @@ impl BlackScholes {
     /// The generalized Black and Scholes formula on variance form.
     #[allow(non_snake_case)]
     pub fn price(is_call: bool, S: f64, K: f64, t: f64, r: f64, b: f64, v: f64) -> f64 {
-        let d1 = (log(S / K) + (b + v / 2.0) * t) / sqrt(v * t);
-        let d2 = d1 - sqrt(v * t);
+        let d1 = ((S / K).ln() + (b + v / 2.0) * t) / (v * t).sqrt();
+        let d2 = d1 - (v * t).sqrt();
 
         if is_call {
-            S * exp((b - r) * t) * cdf(d1) - K * exp(-r * t) * cdf(d2)
+            S * ((b - r) * t).exp() * cdf(d1) - K * (-r * t).exp() * cdf(d2)
         } else {
-            K * exp(-r * t) * cdf(-d2) - S * exp((b - r) * t) * cdf(-d1)
+            K * (-r * t).exp() * cdf(-d2) - S * ((b - r) * t).exp() * cdf(-d1)
         }
     }
 }
